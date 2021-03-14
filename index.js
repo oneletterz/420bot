60eb528107b14d8a85252c0efeb50636d0e1fa16
oneletterz <oneletterz@gmail.com>
2021-03-14 12:19:57 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const bot = require('./bot.js');
+
+const express = require('express')
+const app = express()
+const port = Number(process.env.PORT || 5000)
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World, I\'m 420bot!')
+})
+
+app.post('/', async (req, res) => {
+  console.log(req.body)
+  await bot.respond(req.body, res);
+})
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
